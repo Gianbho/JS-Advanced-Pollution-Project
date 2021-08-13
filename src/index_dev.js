b4a45e6e8850dc0e4f995793cea4dad6d4052a58
Gianbho <gianluca.trm@gmail.com>
2021-08-13 22:25:14 +0200

    3

--- src/index_dev.js
+++ src/index_dev.js
@@ -6,6 +6,7 @@ async function getCoordPollution (lat, lon) {
   let response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=${API_KEY}`);
   if(response.status == 200){
     let result = await response.json();
+    console.log(result);
     let city = result.data.city;
     alert(city.name);
   } else {
@@ -16,11 +17,14 @@ async function getCoordPollution (lat, lon) {
 //fetching data from city input
 async function getCityPollution(city) {
   let response = await fetch(`https://api.waqi.info/feed/${city}/?token=${API_KEY}`);
-  if(response.status == 200) {
-    let result = await response.json();
+  let result = await response.json();
+  if(response.status == 200 && result.status == 'ok') {
     alert(result.data.city.name);
+  } else if (result.status == 'error'){
+    console.log(`${result.status}: ${result.data}`);
+    alert(`Unfortunately we have no datas for ${city} station (incredible but true), insert coords to see datas from the nearest station or try with another city. Go check https://waqi.info/ to see`)
   } else {
-    console.log(`Err ${response.status}: ${response.message}`);
+    console.log(response.status);
   }
 }
 
@@ -34,20 +38,20 @@ getCity.onclick = () => {
 //data output from user coords input
 let getLocalCoords = document.getElementById('getLocalCoords');
 getLocalCoords.onclick = async function getCoord(){
- function success (pos) {
+  let latitude;
+  let longitude;
+  function success (pos) {
    let crd = pos.coords;
    coordInput[0].value = `${crd.latitude}`;
    coordInput[1].value = `${crd.longitude}`;
    latitude = coordInput[0].value;
    longitude = coordInput[1].value;
- };
- function error (err){
-  console.log(`Alert! Error: ${err.code}, ${err.message}`);
- }
+   getCoordPollution(latitude, longitude);
+  };
+  function error (err){
+   console.log(`Alert! Error: ${err.code}, ${err.message}`);
+  }
  navigator.geolocation.getCurrentPosition(success, error);
- let latitude;
- let longitude;
- setTimeout(() => getCoordPollution(latitude, longitude), 300);
 }
 
 //data output from somewhere coords input

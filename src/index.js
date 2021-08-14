8d69b67986cbcc81c20b9aa2e709cd204ce3ee62
Gianbho <gianluca.trm@gmail.com>
2021-08-14 13:40:28 +0200

    3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const API_KEY = process.env.API_KEY;
+import _ from 'lodash';
 let coordInput = document.getElementsByTagName('input');
+
+//add fetch data handler
+
+function dataHandler(json) {
+  const data = _.get(json, "data")
+  const city = _.get(json, "data.city");
+  const coords = _.get(json, "data.city.geo");
+  const pmVal = _.get(json, "data.aqi");
+
+  coordInput[0].value = city.geo[0];
+  coordInput[1].value = city.geo[1];
+  coordInput[2].value = city.name;
+}
 
 //fetching data from coords input
 async function getCoordPollution (lat, lon) {
   let response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=${API_KEY}`);
   if(response.status == 200){
     let result = await response.json();
+    await dataHandler(result);
+    //alert(city.name);
     console.log(result);
-    let city = result.data.city;
-    alert(city.name);
   } else {
     console.log(`Error ${response.status}: ${response.message}`);
   }
@@ -19,23 +33,26 @@ async function getCityPollution(city) {
   let response = await fetch(`https://api.waqi.info/feed/${city}/?token=${API_KEY}`);
   let result = await response.json();
   if(response.status == 200 && result.status == 'ok') {
-    alert(result.data.city.name);
+    dataHandler(result);
   } else if (result.status == 'error'){
     console.log(`${result.status}: ${result.data}`);
-    alert(`Unfortunately we have no datas for ${city} station (incredible but true), insert coords to see datas from the nearest station or try with another city. Go check https://waqi.info/ to see`)
+    coordInput[0].value = "";
+    coordInput[1].value = "";
+    setTimeout(() => alert(`Unfortunately we have no datas for ${city} station (incredible but true), insert coords to see datas from the nearest station or try with another city. Go check https://waqi.info/ to see`)
+    , 150);
   } else {
     console.log(response.status);
   }
 }
 
-// data output from city input
+// getting city input and call output function
 let getCity = document.querySelector('#getCity');
 getCity.onclick = () => {
   let city = coordInput[2].value;
   getCityPollution(city);
 }
 
-//data output from user coords input
+//getting user coords input and call output function
 let getLocalCoords = document.getElementById('getLocalCoords');
 getLocalCoords.onclick = async function getCoord(){
   let latitude;

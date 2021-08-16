b06cffd28163dfbbaf4811403354464e1349ee49
Gianbho <gianluca.trm@gmail.com>
2021-08-17 00:28:15 +0200

    3

--- src/index_dev.js
+++ src/index_dev.js
@@ -3,19 +3,90 @@ import _ from 'lodash';
 import 'bootstrap';
 import 'jquery';
 import './style.css';
-let coordInput = document.getElementsByTagName('input');
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// stupid hack so that leaflet's images work after going through webpack
+import marker from 'leaflet/dist/images/marker-icon.png';
+import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: marker2x,
+    iconUrl: marker,
+    shadowUrl: markerShadow
+});
+
+let coordInput = document.getElementById('input-form').elements;
+let cityInput = document.getElementById('city-selector');
+let dataOutputs = document.getElementById('output-form').elements;
+
+let leafletMap = document.querySelector('map');
+
+
+//creating map
+var map = L.map(leafletMap, {minZoom:3, maxZoom: 8}).setView([45.4654219, 9.1859243], 3);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let mapMarker;
+
+//updating and showing datas on the map
+function mapUpdate(lat, lon, city) {
+  map.setView([lat, lon], 7);
+  if(mapMarker){
+    map.removeLayer(mapMarker);
+  }
+  mapMarker = L.marker([lat, lon]).addTo(map)
+  .bindPopup(`${city}`)
+  .openPopup();
+  map.addLayer(mapMarker);
+}
+
+//getting datas by clicking on map
+
+map.on('click', async function(e) {
+    let lat = e.latlng.lat;
+    let lon = e.latlng.lng;
+    console.log(lat, lon);
+    let response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=${API_KEY}`);
+    if(response.status == 200) {
+      let result = await response.json();
+      dataHandler(result);
+    } else {
+      alert('error handling');
+    };
+});
+
 
 //add fetch data handler
 
 function dataHandler(json) {
   const data = _.get(json, "data")
-  const city = _.get(json, "data.city");
+  const city = _.get(json, "data.city.name");
   const coords = _.get(json, "data.city.geo");
-  const pmVal = _.get(json, "data.aqi");
+  const aqi = _.get(json, "data.aqi");
+  const timeZone = _.get(json, "data.time.tz");
+  const lat = coords[0];
+  const lon = coords[1];
+  const pm25 =_.get(json, "data.forecast.daily.pm25[0].avg")
+  const pm10 =_.get(json, "data.forecast.daily.pm10[0].avg")
+  const uvi = _.get(json, "data.forecast.daily.uvi[0].max")
 
-  coordInput[0].value = city.geo[0];
-  coordInput[1].value = city.geo[1];
-  coordInput[2].value = city.name;
+  coordInput[0].value = coords[0];
+  coordInput[1].value = coords[1];
+  dataOutputs[0].value = city;
+  dataOutputs[1].value = `${coords[0]}, ${coords[1]}`;
+  dataOutputs[2].value = pm25;
+  dataOutputs[3].value = pm10;
+  dataOutputs[4].value = aqi;
+  dataOutputs[5].value = uvi;
+
+  mapUpdate(lat, lon, city);
 }
 
 //fetching data from coords input
@@ -23,8 +94,10 @@ async function getCoordPollution (lat, lon) {
   let response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=${API_KEY}`);
   if(response.status == 200){
     let result = await response.json();
+    cityInput.value = '';
     await dataHandler(result);
-    //alert(coordInput[2].value);
+
+    //alert(cityInput.value);
     console.log(result);
   } else {
     console.log(`Error ${response.status}: ${response.message}`);
@@ -37,6 +110,7 @@ async function getCityPollution(city) {
   let result = await response.json();
   if(response.status == 200 && result.status == 'ok') {
     dataHandler(result);
+    console.log(result);
   } else if (result.status == 'error'){
     console.log(`${result.status}: ${result.data}`);
     coordInput[0].value = "";
@@ -51,8 +125,12 @@ async function getCityPollution(city) {
 // getting city input and call output function
 let getCity = document.querySelector('#getCity');
 getCity.onclick = () => {
-  let city = coordInput[2].value;
-  getCityPollution(city);
+  let city = cityInput.value;
+  if (city) {
+    getCityPollution(city);
+  } else {
+    alert('remember to handle this')
+  }
 }
 
 //getting user coords input and call output function
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = (env, argv) => {
       test: /\.css$/i,
       use: ["style-loader", "css-loader"],
     },
+    {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+    }
   ],
 },
     plugins: [

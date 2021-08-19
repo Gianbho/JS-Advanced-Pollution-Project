09509c8b03a2e10db4c2de23c0bc0ec615b33e13
Gianbho <gianluca.trm@gmail.com>
2021-08-19 15:12:03 +0200

    boh

--- netlify/functions/omega.js
+++ netlify/functions/omega.js
@@ -1,4 +1,4 @@
-exports.handler = async event => {
+exports.handler = async function (lat, lon) => {
   const API_KEY =  process.env.API_KEY
 
   const response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=${API_KEY}`)

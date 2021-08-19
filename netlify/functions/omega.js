216c3ec3020622d6004dfdcc3401fdc86424f93b
Gianbho <gianluca.trm@gmail.com>
2021-08-19 15:32:39 +0200

    .

--- netlify/functions/omega.js
+++ netlify/functions/omega.js
@@ -1,15 +1,15 @@
 exports.handler = async function (lat, lon) => {
-  const API_KEY =  process.env.API_KEY
+  const API_KEY =  process.env.API_KEY;
 
-  const response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=${API_KEY}`)
-  const data = await response.json()
+  const response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=${API_KEY}`);
+  const data = await response.json();
 
   const pass = (body) => {
     return {
       statusCode: 200,
       body: JSON.stringify(body)
     }
-  }
+  };
 
-  return pass(data)
+  return pass(data);
 }

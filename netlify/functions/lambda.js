6d19ef94ca90f3f05713e3f0532cc9ab4011c098
Gianbho <gianluca.trm@gmail.com>
2021-08-20 19:04:13 +0200

    Update lambda.js

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -7,8 +7,8 @@ const fetch = require('node-fetch');
 let city = process.env.CITY;
 exports.handler = async event => {
   const API_KEY =  process.env.API_KEY;
-  const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${API_KEY}`);
-  const data = await response.json();
+  let response = await fetch(`https://api.waqi.info/feed/${city}/?token=${API_KEY}`);
+  let data = await response.json();
 
   const pass = (body) => {
     return {
